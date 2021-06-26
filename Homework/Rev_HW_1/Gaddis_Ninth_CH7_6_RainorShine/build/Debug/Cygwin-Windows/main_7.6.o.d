8f9bafc47559b59bb8e64ef38173fbdb8d276eb1
MartinMcNally1 <mmcnally1@student.rccd.edu>
2021-06-26 10:55:16 -0700

    HW

---
+++ Homework/Rev_HW_1/Gaddis_Ninth_CH7_6_RainorShine/build/Debug/Cygwin-Windows/main_7.6.o.d
@@ -0,0 +1,1 @@
+build/Debug/Cygwin-Windows/main_7.6.o: main_7.6.cpp
--- Homework/Rev_HW_1/Gaddis_Ninth_CH7_6_RainorShine/main_7.6.cpp
+++ Homework/Rev_HW_1/Gaddis_Ninth_CH7_6_RainorShine/main_7.6.cpp
@@ -8,36 +8,34 @@
 #include <iostream>   //I/O Library
 #include <cmath>      //Math Library
 #include <iomanip>    //Formating Output
+#include <fstream>    //Input File streaming Object
 
 using namespace std;  //Library Scope
 
-//User Libraries
-
-//Global Constants
-
-//Science and Math, Conversions, Higher Dimensions
-
-//const to follow
-
-//Function Prototypes
-
 //Execution Starts Here
 int main(){
-    //Set Random Number Seed Here
 
-    //Declare Variables - Known and Unknown, units, range, description
+    int mnth=3;     //0=June, 1=July, 2=August
+    int days=30;    //30 days max
+    int numS;       //Number of Sunny days recorded
+    int numC;       //Number of Cloudy days recorded
+    int numR;       //Number of Rainy days recorded
+    int i=0;
+    ifstream inputFile;
+ 
+    char Wdata[mnth][days];  //3x30 array to hold characters S, C, R
+    
+    inputFile.open("RainOrShine.txt");  //Opens RainOrShine.txt file
+    
+    for (int i2=0; i2<=mnth; i2++){     //Reads data into array
+        while ( i <= days && inputFile >> Wdata[i2][i]){
+            i++;
+        }
+    } 
+    
+    inputFile.close();  //Closes File
 
-    //Initialize Variables
-    
-    //Map inputs to outputs -> i.e. process the inputs
-    
-    //Display the outputs
-    
-    //Clean up - File closing, memory de-allocation, etc....
-
-    //Exit Stage Correctly
+    cout << Wdata[0][0] << endl;
     
     return 0;
 }
-
-//Function Implementations
